e1af266f755670ccac8adcb2d44a661c60f9faa0
walaa obeidat <wal.obeidat@gmail.com>
2022-12-12 22:59:46 +0300

    tasks3

--- task2.php
+++ task2.php
@@ -18,8 +18,12 @@
 // $file_last_modified = filemtime($current_file_name); 
 // echo "Last modified " . date("l, dS F, Y, h:ia", $file_last_modified)."\n";
   echo <"br">
-
-
+//   3-Write a PHP script to count number of lines in a file.
+//   Note: Store a text file name into a variable and count the number of lines of text it has.
+  
+//   $file = basename($_SERVER['PHP_SELF']); 
+//   $no_of_lines = count(file($file)); 
+//   echo "There are $no_of_lines lines in $file"."\n";
 
 
 
